3359002c13904123771e6d022a5bb147957b2d9f
Abdillahi485 <daakircadde143@gmail.com>
2023-12-03 15:51:13 +0300

    finished reverse-words

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+function reverseWords(sentence){
+let arr=sentence.split(' ')
+let reverseSentece=''
+ for(let i=arr.length-1;i>=0;i--){
+    if(arr[i] == ""){continue}
+    if(reverseSentece.length>0)reverseSentece+=" "; 
+    reverseSentece+=arr[i]
+ }
+
+ return reverseSentece;
+}
+
+let result1=reverseWords("fikrcamp bootcamp") 
+let result2=reverseWords("hello there!") 
+console.log(result1)
+console.log(result2)
+
